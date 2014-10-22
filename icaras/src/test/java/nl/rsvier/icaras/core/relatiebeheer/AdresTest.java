77e70709cf9d6f9cceb2ec43e041d69f05641604
Gordon Brouwer <realgordon@gmail.com>
2014-10-22 17:03:13 +0200

    test voor core klassen Adres en Nfa

--- icaras/src/test/java/nl/rsvier/icaras/core/relatiebeheer/AdresTest.java
+++ icaras/src/test/java/nl/rsvier/icaras/core/relatiebeheer/AdresTest.java
@@ -17,31 +17,30 @@ public class AdresTest {
 		standaardAdres = new Adres();
 		standaardAdres.setAdresId(1);
 		standaardAdres.setHuisOfPostbusNummer("123A");
-		standaardAdres.setIsPostbus(false);
+		standaardAdres.maakStraat();
 		standaardAdres.setPlaats("Hilversum");
 		standaardAdres.setPostcode("1234AB");
 		standaardAdres.setStraat("Larenseweg");
 		gelijkAanStandaardAdres = new Adres();
 		gelijkAanStandaardAdres.setAdresId(1);
 		gelijkAanStandaardAdres.setHuisOfPostbusNummer("123A");
-		gelijkAanStandaardAdres.setIsPostbus(false);
+		gelijkAanStandaardAdres.maakStraat();
 		gelijkAanStandaardAdres.setPlaats("Hilversum");
 		gelijkAanStandaardAdres.setPostcode("1234AB");
 		gelijkAanStandaardAdres.setStraat("Larenseweg");
 		metNullAttribuut = new Adres();
 		metNullAttribuut.setAdresId(1);
 		metNullAttribuut.setHuisOfPostbusNummer("123A");
-		metNullAttribuut.setIsPostbus(null);
-		metNullAttribuut.setPlaats("Hilversum");
+		metNullAttribuut.maakStraat();
+		metNullAttribuut.setPlaats(null);
 		metNullAttribuut.setPostcode("1234AB");
 		metNullAttribuut.setStraat("Larenseweg");
 		metEenAfwijkendAttribuut = new Adres();
 		metEenAfwijkendAttribuut.setAdresId(1);
-		metEenAfwijkendAttribuut.setHuisOfPostbusNummer("123");
-		metEenAfwijkendAttribuut.setIsPostbus(false);
+		metEenAfwijkendAttribuut.setHuisOfPostbusNummer("123A");
+		metEenAfwijkendAttribuut.maakPostbus();
 		metEenAfwijkendAttribuut.setPlaats("Hilversum");
 		metEenAfwijkendAttribuut.setPostcode("1234AB");
-		metEenAfwijkendAttribuut.setStraat("Larenseweg");
 	}
 	
 	@Test
@@ -49,8 +48,27 @@ public class AdresTest {
 		assertTrue("Vergeleken met zichzelf", standaardAdres.equals(standaardAdres));
 		assertTrue("Vergeleken met adres met dezelfde attributen", standaardAdres.equals(gelijkAanStandaardAdres));
 		assertFalse("Vergeleken met ander soort object", standaardAdres.equals("ander Object"));
-		assertFalse("isPostbus is null", standaardAdres.equals(metNullAttribuut));
-		assertFalse("Vergeleken met huisnummer zonder toevoeging A", standaardAdres.equals(metEenAfwijkendAttribuut));
+		assertFalse("Plaats is null", standaardAdres.equals(metNullAttribuut));
+		assertFalse("Vergeleken met zelfde adres als postbus", standaardAdres.equals(metEenAfwijkendAttribuut));
+	}
+	
+	@Test
+	public void testMaakPostbus(){
+		standaardAdres.maakPostbus();
+		assertTrue("De boolean isPostbus is nu true", standaardAdres.getIsPostbus());
+		assertTrue("straatnaam is veranderd in \"nvt\"", standaardAdres.getStraat().equals("nvt"));
+	}
+	
+	@Test
+	public void testMaakStraat(){
+		metEenAfwijkendAttribuut.maakStraat();
+		assertFalse("boolean isPostbus is false", metEenAfwijkendAttribuut.getIsPostbus());
+		assertTrue("Straatnaam is gewijzigd van \"nvt\" naar lege String", metEenAfwijkendAttribuut.getStraat().equals(""));
+		Adres adresVoorWijziging = standaardAdres;
+		standaardAdres.maakPostbus();
+		standaardAdres.maakStraat();
+		assertTrue("Na maakPostbus() en terug via maakStraat() alles ongewijzigd", adresVoorWijziging.equals(standaardAdres));
+		
 	}
 
 }
