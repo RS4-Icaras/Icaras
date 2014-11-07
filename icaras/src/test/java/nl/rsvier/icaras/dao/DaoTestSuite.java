79d042239fa30f7cd611ccdf9eb0d348f999ad33
Gordon Brouwer <realgordon@gmail.com>
2014-11-07 15:04:57 +0100

    testklassen toegevoegd aan de suite

--- icaras/src/test/java/nl/rsvier/icaras/dao/DaoTestSuite.java
+++ icaras/src/test/java/nl/rsvier/icaras/dao/DaoTestSuite.java
@@ -1,23 +1,35 @@
 package nl.rsvier.icaras.dao;
 
 import nl.rsvier.icaras.dao.relatiebeheer.AdresDaoHibernateTest;
+import nl.rsvier.icaras.dao.relatiebeheer.ArbeidsovereenkomstDaoHibernateTest;
+import nl.rsvier.icaras.dao.relatiebeheer.KandidaatDaoHibernateTest;
 import nl.rsvier.icaras.dao.relatiebeheer.NfaDaoHibernateTest;
+import nl.rsvier.icaras.dao.relatiebeheer.PersoonDaoHibernateTest;
 import nl.rsvier.icaras.dao.relatiebeheer.RelatieDaoHibernateTest;
+import nl.rsvier.icaras.dao.relatiebeheer.WerknemerDaoHibernateTest;
+import nl.rsvier.icaras.service.relatiebeheer.PersoonOrganisatiemoduleServiceTest;
+import nl.rsvier.icaras.service.relatiebeheer.RelatieServiceTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 /**
  * 
- * @author Gerben en Gordon
- * Deze klasse is bedoeld om alle DaoTests gezamenlijk te runnen binnen dezelfde Spring-test.
- * Het opstarten van de ApplicationContext (inclusief het genereren van het databaseschema)
- * hoeft hierdoor maar eenmalig te gebeuren voor alle tests.
+ * @author Gerben en Gordon Deze klasse is bedoeld om alle DaoTests gezamenlijk
+ *         te runnen binnen dezelfde Spring-test. Het opstarten van de
+ *         ApplicationContext (inclusief het genereren van het databaseschema)
+ *         hoeft hierdoor maar eenmalig te gebeuren voor alle tests.
  *
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({AdresDaoHibernateTest.class, NfaDaoHibernateTest.class, RelatieDaoHibernateTest.class})//Voeg hier alle DaoTest-klassen aan toe
-//@ContextConfiguration(locations={"classpath:icarasdb-context.xml"})//Per testklasse toevoegen ivm autowiring
+@Suite.SuiteClasses({ AdresDaoHibernateTest.class, NfaDaoHibernateTest.class,
+		RelatieDaoHibernateTest.class, RelatieServiceTest.class,
+		ArbeidsovereenkomstDaoHibernateTest.class,
+		KandidaatDaoHibernateTest.class, PersoonDaoHibernateTest.class,
+		WerknemerDaoHibernateTest.class, PersoonOrganisatiemoduleServiceTest.class })
+// Voeg hier alle DaoTest-klassen aan toe
+// @ContextConfiguration(locations={"classpath:icarasdb-context.xml"})//Per
+// testklasse toevoegen ivm autowiring
 public class DaoTestSuite {
 
 }
