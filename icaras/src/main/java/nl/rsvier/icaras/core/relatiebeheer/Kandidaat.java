a8fb5f5efda4223a853cc1fa959869ec5798293d
Gordon Brouwer <realgordon@gmail.com>
2014-11-11 11:52:41 +0100

    methoden voor bidirectionele relaties aangepast

--- icaras/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Kandidaat.java
+++ icaras/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Kandidaat.java
@@ -7,9 +7,6 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-
 import nl.rsvier.icaras.core.arbeidsmarkt.Aanbieding;
 import nl.rsvier.icaras.core.arbeidsmarkt.CvGenerator;
 
@@ -38,33 +35,96 @@ public class Kandidaat extends PersoonsRol {
 	 */
 
 	@OneToMany(orphanRemoval = true, cascade = javax.persistence.CascadeType.ALL)
-	@Cascade(value = { CascadeType.SAVE_UPDATE })
+//	@Cascade(value = { CascadeType.SAVE_UPDATE })
 	public Set<Aanbieding> getAanbiedingen() {
 		return aanbiedingen;
 	}
 
-	public void setAanbiedingen(Set<Aanbieding> aanbiedingen) {
+	@SuppressWarnings("unused")
+	private void setAanbiedingen(Set<Aanbieding> aanbiedingen) {
 		this.aanbiedingen = aanbiedingen;
 	}
 
-	public boolean addAanbieding(Aanbieding aanbieding) {
-		boolean isToegevoegd = false;
-		if (!this.getAanbiedingen().contains(aanbieding)) {
-			isToegevoegd = this.aanbiedingen.add(aanbieding);
-			aanbieding.setPersoonReferentie(this.getCvGenerator().getPersoon());
+	/**
+	 * Voegt de gegeven aanbieding toe aan de lijst van deze kandidaat. De
+	 * aanbieding wordt alleen toegevoegd als deze niet null is, nog niet
+	 * voorkomt in de lijst en aan zowel een organisatie met bedrijfsrol als aan
+	 * de juiste persoon refereert.
+	 * 
+	 * 
+	 * De aanbieding wordt tevens toegevoegd aan het bedrijf van de organisatie
+	 * van deze aanbieding.
+	 * 
+	 * @param aanbieding
+	 *            De toe te voegen aanbieding. Deze aanbieding moet een
+	 *            referentie hebben naar de persoon die eigenaar is van de
+	 *            kandidaat waarop deze methode wordt aangeroepen. De aanbieding
+	 *            moet ook een referentie hebben naar een organisatie met een
+	 *            bedrijfsrol.
+	 * @return true als na afloop de aanbieding voorkomt in de aanbiedingen van
+	 *         zowel de kandidaat als van het bedrijf. Anders false.
+	 */
+	public synchronized boolean addAanbieding(Aanbieding aanbieding) {
+		if (aanbieding == null || aanbieding.getPersoon() == null
+				|| aanbieding.getOrganisatie() == null
+				|| aanbiedingen.contains(aanbieding)) {
+			return false;
 		}
-		return isToegevoegd;
+		Kandidaat kandidaat = aanbieding.getPersoon().getKandidaat();
+		Bedrijf bedrijf = aanbieding.getOrganisatie().getBedrijf();
+		if (kandidaat != null && bedrijf != null && kandidaat == this) {
+			aanbiedingen.add(aanbieding);
+			bedrijf.addAanbieding(aanbieding);
+			return aanbiedingen.contains(aanbieding)
+					&& bedrijf.getAanbiedingen().contains(aanbieding);
+		}
+		return false;
 	}
 
-	public boolean removeAanbieding(Aanbieding aanbieding) {
-		return this.aanbiedingen.remove(aanbieding);
+	/**
+	 * TODO bij het verwijderen van een aanbieding die ook in een
+	 * arbeidsovereenkomst zit, dient deze ook verwijderd te worden! Verwijderd
+	 * de gegeven aanbieding uit de lijst van deze kandidaat indien aanwezig. De
+	 * aanbieding wordt tevens verwijderd bij het bedrijf van de organisatie van
+	 * deze aanbieding.
+	 * 
+	 * @param aanbieding
+	 *            De te verwijderen aanbieding. Deze aanbieding moet een
+	 *            referentie hebben naar de persoon die eigenaar is van de
+	 *            kandidaat waarop deze methode wordt aangeroepen. De aanbieding
+	 *            moet ook een referentie hebben naar een organisatie met een
+	 *            bedrijfsrol.
+	 * @return true als na afloop de aanbieding niet voorkomt in de aanbiedingen
+	 *         van zowel de kandidaat als van het bedrijf. Anders false.
+	 */
+	public synchronized boolean removeAanbieding(Aanbieding aanbieding) {
+		if (aanbieding == null || aanbieding.getPersoon() == null
+				|| aanbieding.getOrganisatie() == null
+				|| !aanbiedingen.contains(aanbieding)) {
+			return false;
+		}
+		Kandidaat kandidaat = aanbieding.getPersoon().getKandidaat();
+		Bedrijf bedrijf = aanbieding.getOrganisatie().getBedrijf();
+		if (kandidaat != null && bedrijf != null && kandidaat == this) {
+			aanbiedingen.remove(aanbieding);
+			bedrijf.removeAanbieding(aanbieding);
+			return !aanbiedingen.contains(aanbieding)
+					&& !bedrijf.getAanbiedingen().contains(aanbieding);
+		}
+		return false;
 	}
+
+	public boolean heeftAanbieding(Aanbieding aanbieding) {
+		return getAanbiedingen().contains(aanbieding);
+	}
+
 	@Embedded
 	public CvGenerator getCvGenerator() {
-		return cvGenerator ;
+		return cvGenerator;
 	}
 
-	@SuppressWarnings("unused") //de methode wordt gebruikt door Hibernate
+	@SuppressWarnings("unused")
+	// de methode wordt gebruikt door Hibernate
 	private void setCvGenerator(CvGenerator cvGenerator) {
 		this.cvGenerator = cvGenerator;
 	}
