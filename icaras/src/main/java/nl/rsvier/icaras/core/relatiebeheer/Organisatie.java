d766a3386ee71722cb15ccaa4be824bb86ef524a
Gordon Brouwer <realgordon@gmail.com>
2014-11-11 11:49:41 +0100

    organisatie van mark overgenomen

--- icaras/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Organisatie.java
+++ icaras/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Organisatie.java
@@ -1,6 +1,8 @@
 package nl.rsvier.icaras.core.relatiebeheer;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -10,6 +12,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
+
+import nl.rsvier.icaras.core.InvalidBusinessKeyException;
 
 /**
  * Core klasse: Organisatie. Entiteit die een zakelijke relatie vertegenwoordigt
@@ -28,22 +32,26 @@ public class Organisatie extends Relatie {
 	private static final long serialVersionUID = 1L;
 	private Set<OrganisatieRol> rollen;
 	private String naam;
+	private List<Persoon> contactpersonen; // Kijk, een lijst!
 
 	/*
 	 * Constructoren
 	 */
 
-	public Organisatie(String s) {
+	public Organisatie(String str) throws InvalidBusinessKeyException {
 		this();
-		this.setNaam(s);
+		if (this.naamMagWordenToegevoegd(str)) {
+			this.setNaam(str);
+		}
 	}
 
-	public Organisatie() {
+	private Organisatie() {
 		/*
 		 * TODO: Make sure each instance of Organisatie has at least one type of
 		 * OrganisatieRol assigned to it? [1..2] vs [0..2]?
 		 */
 		this.rollen = new HashSet<OrganisatieRol>();
+		this.contactpersonen = new ArrayList<Persoon>();
 	}
 
 	/*
@@ -55,18 +63,19 @@ public class Organisatie extends Relatie {
 		return this.rollen;
 	}
 
-	public void setRollen(Set<OrganisatieRol> rollenlijst) {
+	@SuppressWarnings("unused")
+	private void setRollen(Set<OrganisatieRol> rollenlijst) {
 		this.rollen = rollenlijst;
 	}
 
-	/*
+	/**
 	 * Controleer of de collectie daadwerkelijk een rol bevat van het type
 	 * OrganisatieRol
 	 * 
 	 * @param: class literal van het gewenste type OrganisatieRol
 	 * 
 	 * @return: true wanneer de collectie een OrganisatieRol van het juiste type
-	 * bevat
+	 *          bevat
 	 */
 	public <T extends OrganisatieRol> boolean hasRol(Class<T> clstype) {
 		for (OrganisatieRol rol : this.getRollen()) {
@@ -76,71 +85,130 @@ public class Organisatie extends Relatie {
 		}
 		return false;
 	}
-	
-	@Transient
-	public Bedrijf getBedrijf() {
-		return ((Bedrijf) this.getRol(Bedrijf.class));
-	}
-	
-	@Transient
-	public Leverancier getLeverancier() {
-		return ((Leverancier) this.getRol(Leverancier.class));
-	}
-	
 
-	/*
+	/**
 	 * Voeg een OrganisatieRol toe aan de collectie. Er kan slechts 1 rol van
 	 * elke subklasse van OrganisatieRol worden toegevoegd
 	 */
 	public boolean addRol(OrganisatieRol rol) {
-		if (!this.hasRol(rol.getClass())) {
+		if (rol != null && !this.hasRol(rol.getClass())) {
 			return this.getRollen().add(rol);
 		}
 		return false;
 	}
 
-	/*
+	/**
 	 * Retrieves the first element of List<OrganisatieRol> that matches the type
 	 * we're requesting. Considering we're using a Collection type that doesn't
-	 * allow duplicates that should be enough.
-	 * 
-	 * OR
-	 * 
-	 * voorlopig even door de hele collectie loopen om een extra check te doen.
-	 * het zou niet moeten mogen voorkomen dat een organisatie meerdere rollen
-	 * van hetzelfde type bevat
+	 * allow duplicates and we check uniqueness before adding a new Rol that
+	 * should be enough.
 	 */
-
 	public <T extends OrganisatieRol> OrganisatieRol getRol(Class<T> clstype) {
-		int counter = 0;
-		OrganisatieRol tmp = null;
 		for (OrganisatieRol rol : this.getRollen()) {
 			if (clstype.isInstance(rol)) {
-				tmp = rol;
-				if (counter++ > 1) {
-					throw new RuntimeException(
-							"Meerdere OrganisatieRollen van type: "
-									+ rol.getClass().getName()
-									+ " gevonden binnen de rollen collectie van Organisatie. Dit zou niet mogen voorkomen!");
-				}
+				return rol;
 			}
 		}
-		return tmp;
+		return null;
+	}
+
+	@Transient
+	public Bedrijf getBedrijf() {
+		return (Bedrijf) this.getRol(Bedrijf.class);
+	}
+
+	@Transient
+	public Leverancier getLeverancier() {
+		return (Leverancier) this.getRol(Leverancier.class);
 	}
 
 	/*
 	 * Naam: Maak een naam uniek en onaanpasbaar zodat deze te gebruiken is als
 	 * business logic key
 	 */
-
 	@Column(unique = true, updatable = false)
 	@NotNull
 	public String getNaam() {
-		return this.naam; // TODO: robuust maken. ensure naam is unique
+		return this.naam;
 	}
 
-	public void setNaam(String naam) {
+	private void setNaam(String naam) {
 		this.naam = naam;
+	}
+
+	public static boolean naamConstraint(String str) {
+		/*
+		 * De voorwaarden waar een Naam aan moet voldoen
+		 */
+		return str != null && !str.equals("");
+	}
+
+	public boolean naamMagWordenToegevoegd(String str) {
+		/*
+		 * Om een Naam toe te voegen mag deze geen null zijn en moet deze aan de
+		 * voorwaarde voldoen
+		 */
+		return !this.naamIsToegevoegd() && Organisatie.naamConstraint(str);
+	}
+
+	public boolean naamIsToegevoegd() {
+		/*
+		 * Naam is immutable. Als deze reeds is toegevoegd mag deze niet meer
+		 * worder aangepast
+		 */
+		return this.getNaam() != null;
+	}
+
+	/*
+	 * Collectie: Contactpersonen
+	 */
+
+	@OneToMany(orphanRemoval = true, cascade = javax.persistence.CascadeType.ALL)
+	public List<Persoon> getContactpersonen() {
+		return contactpersonen;
+	}
+
+	@SuppressWarnings("unused")
+	private void setContactpersonen(List<Persoon> contactpersonen) {
+		this.contactpersonen = contactpersonen;
+	}
+
+	public boolean addContactpersoon(Persoon persoon) {
+		if (this.contactpersoonMagWordenToegevoegd(persoon)) {
+			// Voeg Persoon toe aan de lijst van contactpersonen
+			boolean a = this.getContactpersonen().add(persoon);
+			// Vertel contactpersoon over deze organisatie
+			boolean b = persoon.getContactpersoon().addOrganisatie(this,
+					persoon);
+			return a && b;
+		}
+		return false;
+	}
+
+	public boolean removeContactpersoon(Persoon persoon) {
+		// als this.getContactpersonen() null is > nooit null, hoogstends leeg
+		// als persoon null is > return false
+		// als persoon geen contactpersoon rol heeft > is al afgevangen in de
+		// add methode
+		return this.getContactpersonen().remove(persoon);
+	}
+
+	public boolean contactpersoonIsToegevoegd(Persoon persoon) {
+		return this.getContactpersonen().contains(persoon);
+	}
+
+	public boolean contactpersoonConstraint(Persoon persoon) {
+		/*
+		 * De voorwaarden waar een Contactpersoon aan moet voldoen om te worden
+		 * toegevoegd
+		 */
+		return persoon != null && !this.contactpersoonIsToegevoegd(persoon)
+				&& persoon.hasRol(Contactpersoon.class);
+	}
+
+	public boolean contactpersoonMagWordenToegevoegd(Persoon persoon) {
+		return !this.contactpersoonIsToegevoegd(persoon)
+				&& this.contactpersoonConstraint(persoon);
 	}
 
 	/*
@@ -151,8 +219,9 @@ public class Organisatie extends Relatie {
 	public int hashCode() {
 		final int prime = 67;
 		int hash = 1;
-		hash = prime * hash
-				+ ((this.getNaam() == null) ? 0 : this.getNaam().hashCode());
+		hash = prime * hash + this.getNaam().hashCode(); // TODO: We need a
+															// better business
+															// key than this
 		return hash;
 	}
 
@@ -164,7 +233,7 @@ public class Organisatie extends Relatie {
 			return false;
 		} else {
 			Organisatie other = (Organisatie) obj;
-			if (!this.getNaam().equals(other.getNaam())) {//TODO nullpointer als naam null is
+			if (!this.getNaam().equals(other.getNaam())) {
 				return false;
 			}
 		}
