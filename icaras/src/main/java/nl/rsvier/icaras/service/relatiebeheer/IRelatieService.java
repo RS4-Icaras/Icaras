365df9bd73b27d0d6f1d700d78f16fbc09c40db0
Gordon Brouwer <realgordon@gmail.com>
2014-11-13 10:49:14 +0100

    relatieService klasse en bijbehorende interface toegevoegd

---
+++ icaras/src/main/java/nl/rsvier/icaras/service/relatiebeheer/IRelatieService.java
@@ -0,0 +1,34 @@
+package nl.rsvier.icaras.service.relatiebeheer;
+
+import java.util.List;
+import java.util.Set;
+
+import nl.rsvier.icaras.core.relatiebeheer.Adres;
+import nl.rsvier.icaras.core.relatiebeheer.Relatie;
+import nl.rsvier.icaras.dao.relatiebeheer.RelatieDaoHibernate;
+
+
+public interface IRelatieService  {
+	
+	public List<Relatie> getAll();
+	
+	public void save(Relatie r);
+	
+	public void delete(Relatie r);
+	
+	public void update(Relatie r);
+	
+	public Relatie getById(int id);
+	
+	public RelatieDaoHibernate getDao();
+	
+	public void setDao(RelatieDaoHibernate dao);
+	
+	public List<Relatie> getAllMetAdres();
+	
+	public Set<Adres> getRelatieAdressen(Relatie r);
+	
+	public Relatie getByIdMetAdres(int id);
+	
+
+}
---
+++ icaras/src/main/java/nl/rsvier/icaras/service/relatiebeheer/RelatieService.java
@@ -0,0 +1,137 @@
+package nl.rsvier.icaras.service.relatiebeheer;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+
+import javax.transaction.Transactional;
+
+import nl.rsvier.icaras.core.relatiebeheer.Adres;
+import nl.rsvier.icaras.core.relatiebeheer.Nfa;
+import nl.rsvier.icaras.core.relatiebeheer.Relatie;
+import nl.rsvier.icaras.dao.relatiebeheer.RelatieDaoHibernate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+/**
+ * Servicelaag voor Relatie klasse Let op: klasse zelf kan niet ge-autowired
+ * worden door Spring, ivm Transactional en proxy pattern. Wil je deze klasse autowiren, Autowire dan de
+ * interface IRelatieService. Deze heeft dezelfde methoden als
+ * RelatieService.
+ * 
+ * @author Gerben
+ *
+ */
+@Service("IRelatieService")
+@Transactional
+public class RelatieService implements IRelatieService {
+
+	@Autowired
+	private RelatieDaoHibernate relatieDao;
+
+	/**
+	 * @return relatieDao
+	 */
+
+	public RelatieDaoHibernate getDao() {
+		return relatieDao;
+	}
+
+	/**
+	 * 
+	 */
+	public void setDao(RelatieDaoHibernate dao) {
+		this.relatieDao = dao;
+	}
+
+	/**
+	 * @return lijst met all relatieObjecten uit de database
+	 */
+	public List<Relatie> getAll() {
+
+		return relatieDao.getAll();
+	}
+
+	/**
+	 * saved meegegeven Relatie attribuut in de database
+	 * Controleert eerst of de adres en nfa Sets null zijn
+	 * als dit zo is wordt lege HashSet aan dit attribuut toegekend
+	 * zodat dataIntegrity gehandhaafd blijft
+	 * 
+	 * @param r
+	 *            de te saven relatie
+	 */
+	public void save(Relatie r) {
+		
+		relatieDao.save(r);
+
+	}
+
+	/**
+	 * verwijderd meegegeven Relatie uit de database
+	 * 
+	 * @param r
+	 *            de te deleten Relatie
+	 */
+	public void delete(Relatie r) {
+		relatieDao.delete(r);
+
+	}
+
+	/**
+	 * update meegegeven relatie
+	 */
+	public void update(Relatie r) {
+		relatieDao.update(r);
+
+	}
+
+	/**
+	 * geeft relatie met meegegeven id
+	 * 
+	 * @return de gevonden relatie met gegeven id, zonder initialisatie van
+	 *         lijsten dit is null als geen relatie gevonden wordt
+	 */
+	public Relatie getById(int id) {
+
+		return relatieDao.getById(id);
+	}
+
+	
+	
+	public Relatie getByIdMetAdres(int id) {
+
+		Relatie r =  relatieDao.getById(id);
+		relatieDao.getHibernateTemplate().initialize(r.getAdressen());
+		return r;
+		
+	}
+
+	/**
+	 * @return lijst met alle relaties in db, in elke relatie is ook de
+	 *         adreslijst geinstantieerd
+	 */
+	 public List<Relatie> getAllMetAdres() {
+
+		List<Relatie> relatieLijst = relatieDao.getAll();
+		ListIterator<Relatie> listitr = relatieLijst.listIterator();
+
+		while (listitr.hasNext()) {
+			Relatie r = (Relatie) listitr.next();
+			relatieDao.getHibernateTemplate().initialize(r.getAdressen());
+		}
+
+		return relatieLijst;
+
+	}
+
+	public Set<Adres> getRelatieAdressen(Relatie r) {
+		relatieDao.getHibernateTemplate().initialize(r.getAdressen());
+		return r.getAdressen();
+		
+	}
+
+}
