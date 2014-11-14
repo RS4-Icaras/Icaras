d65be73a13a91f4ee89b4d7a9bcb31849285a97e
Gordon Brouwer <realgordon@gmail.com>
2014-11-14 09:38:39 +0100

    Een relatie heeft nu een correspondentieadres. Klasse Adres heef nu boolean isCorrespondentieadres. Klasse Relatie beheert samen met klasse Adres dat zolang er een of meerdere adressen aanwezig zijn, er exact 1 correspondentieadres is. Klasse Relatie heeft nu private methoden voor setAdressen en setNfaLijst. Klasse Relatie heeft addAdres-, addNfa- en bijbehorende removeMethoden. Testklasse toegevoegd.

--- icaras/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Adres.java
+++ icaras/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Adres.java
@@ -9,27 +9,48 @@ import nl.rsvier.icaras.core.IEntity;
 
 /**
  * 
- * @author Gerben en Gordon 
- * Klasse voor fysieke adressen 
- * Dit kan een postbus of een adres met een straatnaam zijn. Standaard is adres met straatnaam.
- * Gebruik de hulpmethode maakPostbus voor een postbus en maakStraat om dit weer ongedaan te maken.
+ * Klasse voor fysieke adressen. Dit kan een postbus of een adres met een
+ * straatnaam zijn. Standaard is adres met straatnaam. Gebruik de hulpmethode
+ * maakPostbus voor een postbus en maakStraat om dit weer ongedaan te maken. Een
+ * adres kan een correspondentieadres zijn. Dit kan slechts voor 1 adres gelden
+ * in de adressenlijst van een Relatie. De meest recente toewijzing als
+ * correspondentieadres overschrijft eventuele eerdere toewijzingen.
  *
+ * @author Gerben
+ * @author Gordon
  */
 @Entity
 public class Adres implements IEntity {
 
 	private static final long serialVersionUID = 1L;
 	private int id;
-	private Boolean isPostbus;// = false;//als alternatief voor constructor, kan beide.
+	private boolean isCorrespondentieAdres;
+	private boolean isPostbus;
 	private String straat;
 	private String huisOfPostbusNummer;
 	private String postcode;
 	private String plaats;
 	@Transient
 	private String straatVoorPostbus;
-	
-	public Adres(){
-		isPostbus = false;
+
+	public Adres() {
+		setIsPostbus(false);
+		setCorrespondentieAdres(false);
+	}
+
+	public Adres(boolean isCorrespondentieAdres, boolean isPostbus,
+			String postcode, String huisOfPostbusNummer, String plaats,
+			String straat) {
+		if (isPostbus) {
+			maakPostbus();
+		} else {
+			setIsPostbus(false);
+			setStraat(straat);
+		}
+		setCorrespondentieAdres(isCorrespondentieAdres);
+		setPostcode(postcode);
+		setHuisOfPostbusNummer(huisOfPostbusNummer);
+		setPlaats(plaats);
 	}
 
 	/**
@@ -49,77 +70,116 @@ public class Adres implements IEntity {
 		this.id = id;
 	}
 
+	public boolean isCorrespondentieAdres() {
+		return isCorrespondentieAdres;
+	}
+
+	private void setCorrespondentieAdres(boolean isCorrespondentieAdres) {
+		this.isCorrespondentieAdres = isCorrespondentieAdres;
+	}
+
 	/**
-	 * @return the isPostbus
+	 * Maakt van dit adres een correspondentieadres mits de opgegeven relatie
+	 * eigenaar is van deze adresinstantie. Aanroep kan dus pas na het toevoegen
+	 * van een adres.
+	 * 
+	 * Zorgt er tevens voor dat dit adres het enige correspondentieadres in de
+	 * lijst van de betreffende relatie is.
+	 * 
+	 * @param relatie
+	 *            De relatie moet eigenaar zijn van de adresinstantie waarop
+	 *            deze methode wordt aangeroepen, anders doet de methode niets.
+	 * @return True als de juiste persoon is opgegeven, of het adres al
+	 *         correspondentieadres was, anders false.
 	 */
+	public boolean maakCorrespondentieAdres(Relatie relatie) {
+		if (isCorrespondentieAdres) {
+			return true;
+		}
+		if (relatie.heeftAdres(this)) {
+			setCorrespondentieAdres(true);
+			// mogelijk nu 2 correspondentieadressen
+			// verwijder de andere als dit het geval is
+			for (Adres r : relatie.getAdressen()) {
+				if (r.isCorrespondentieAdres && !r.equals(this)) {
+					r.geenCorrespondentieAdres(relatie);
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * LET OP: Dit is slechts een hulpmethode die nooit zal slagen indien deze
+	 * zelf wordt aangeroepen! Gebruik de Methode maakCorrespondentieAdres om
+	 * indirect alle andere adressen van de betreffende relatie tot
+	 * niet-correspondentieadres te maken.
+	 * 
+	 * LET OP: Werkt enkel als de methode addAdres tijdelijk een 2e
+	 * correspondentieadres toevoegt alvorens deze methode aan te roepen en is
+	 * dus enkel voor die methode bedoeld. Moet echter public zijn vanwege het
+	 * zich in een andere klasse (Relatie) bevinden van die methode.
+	 * 
+	 * @param relatie
+	 *            De relatie moet eigenaar zijn van de adresinstantie waarop
+	 *            deze methode wordt aangeroepen, anders doet de methode niets.
+	 * @return True als de juiste persoon is opgegeven, of het adres al geen
+	 *         correspondentieadres was, anders false.
+	 */
+	public boolean geenCorrespondentieAdres(Relatie relatie) {
+		if (!isCorrespondentieAdres) {
+			return true;
+		}
+		if (relatie.heeftAdres(this)) {
+			for (Adres r : relatie.getAdressen()) {
+				if (r.isCorrespondentieAdres && !r.equals(this)) {
+					setCorrespondentieAdres(false);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 	public Boolean getIsPostbus() {
 		return isPostbus;
 	}
 
-	/**
-	 * @param isPostbus
-	 *            the isPostbus to set
-	 */
-	private void setIsPostbus(Boolean isPostbus) {//private ter verplichting van maakStraat() en maakPostbus()
+	private void setIsPostbus(Boolean isPostbus) {// private ter verplichting
+													// van maakStraat() en
+													// maakPostbus()
 		this.isPostbus = isPostbus;
 	}
 
-	/**
-	 * @return the straat
-	 */
 	public String getStraat() {
 		return straat;
 	}
 
-	/**
-	 * @param straat
-	 *            the straat to set
-	 */
 	public void setStraat(String straat) {
 		this.straat = straat;
 	}
 
-	/**
-	 * @return the huisOfPostbusNummer
-	 */
 	public String getHuisOfPostbusNummer() {
 		return huisOfPostbusNummer;
 	}
 
-	/**
-	 * @param huisOfPostbusNummer
-	 *            the huisOfPostbusNummer to set
-	 */
 	public void setHuisOfPostbusNummer(String huisOfPostbusNummer) {
 		this.huisOfPostbusNummer = huisOfPostbusNummer;
 	}
 
-	/**
-	 * @return the postcode
-	 */
 	public String getPostcode() {
 		return postcode;
 	}
 
-	/**
-	 * @param postcode
-	 *            the postcode to set
-	 */
 	public void setPostcode(String postcode) {
 		this.postcode = postcode;
 	}
 
-	/**
-	 * @return the plaats
-	 */
 	public String getPlaats() {
 		return plaats;
 	}
 
-	/**
-	 * @param plaats
-	 *            the plaats to set
-	 */
 	public void setPlaats(String plaats) {
 		this.plaats = plaats;
 	}
@@ -150,19 +210,20 @@ public class Adres implements IEntity {
 			straat = straatVoorPostbus;
 		}
 	}
-	
+
 	public boolean equals(Object obj) {
 		boolean isEqual = false;
 		if (obj instanceof Adres
-			&& this.id == ((Adres) obj).getId()
-			&& this.isPostbus == ((Adres) obj).getIsPostbus()
-			&& this.straat.equals(((Adres) obj).getStraat())
-			&& this.huisOfPostbusNummer.equals(((Adres) obj).getHuisOfPostbusNummer())
-			&& this.postcode.equals(((Adres) obj).getPostcode())
-			&& this.plaats.equals(((Adres) obj).getPlaats())) {
+				&& this.id == ((Adres) obj).getId()
+				&& this.isPostbus == ((Adres) obj).getIsPostbus()
+				&& this.straat.equals(((Adres) obj).getStraat())
+				&& this.huisOfPostbusNummer.equals(((Adres) obj)
+						.getHuisOfPostbusNummer())
+				&& this.postcode.equals(((Adres) obj).getPostcode())
+				&& this.plaats.equals(((Adres) obj).getPlaats())) {
 			isEqual = true;
 		}
-		return isEqual;	
+		return isEqual;
 	}
 
 }
--- icaras/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Relatie.java
+++ icaras/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Relatie.java
@@ -10,24 +10,33 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
 import nl.rsvier.icaras.core.IEntity;
 
-//TODO methoden voor toevoegen van adressen en private maken van setMethoden voor lijsten
+/**
+ * Abstracte klasse die het beheer van de adressen en Nfa's (niet fysieke
+ * adressen) regelt voor de subklassen Persoon en Organisatie. Zorgt samen met
+ * de klasse Adres voor de garantie van precies 1 correspondentieadres.
+ * 
+ * @author Gerben
+ * @author Gordon
+ *
+ */
 @Entity
-@Inheritance(strategy=InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Relatie implements IEntity {
 
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	private int id;
 	private boolean gearchiveerd = false;
 	private String opmerking;
 	private boolean priveRelatie = false;
 	private Set<Nfa> nfaLijst = new HashSet<Nfa>();
 	private Set<Adres> adressen = new HashSet<Adres>();
+
 	/**
 	 * @return the relatieId
 	 */
@@ -36,75 +45,155 @@ public abstract class Relatie implements IEntity {
 	public int getId() {
 		return id;
 	}
+
 	/**
-	 * @param relatieId the relatieId to set
+	 * @param relatieId
+	 *            the relatieId to set
 	 */
 	public void setId(int relatieId) {
 		this.id = relatieId;
 	}
-	public boolean isGearchiveerd() {//moeten de rollen van de te archiveren persoon ook automatisch gearchiveerd worden?
+
+	public boolean isGearchiveerd() {// moeten de rollen van de te archiveren
+										// persoon ook automatisch gearchiveerd
+										// worden?
 		return gearchiveerd;
 	}
+
 	public void setGearchiveerd(boolean gearchiveerd) {
 		this.gearchiveerd = gearchiveerd;
 	}
+
 	public String getOpmerking() {
 		return opmerking;
 	}
+
 	public void setOpmerking(String opmerking) {
 		this.opmerking = opmerking;
 	}
+
 	public boolean isPriveRelatie() {
 		return priveRelatie;
 	}
+
 	public void setPriveRelatie(boolean priveRelatie) {
 		this.priveRelatie = priveRelatie;
 	}
+
 	/**
 	 * @return the nfaLijst
 	 */
-	@NotNull//Wat doet dit precies?
+	@NotNull
+	// Wat doet dit precies?
 	@OneToMany(cascade = CascadeType.ALL)
 	public Set<Nfa> getNfaLijst() {
 		return nfaLijst;
 	}
-	/**
-	 * @param nfaLijst the nfaLijst to set
-	 */
+
 	@SuppressWarnings("unused")
-	public void setNfaLijst(Set<Nfa> nfaLijst) {
+	private void setNfaLijst(Set<Nfa> nfaLijst) {
 		this.nfaLijst = nfaLijst;
 	}
-	/**
-	 * @return the adressen
-	 */
+
 	@NotNull
 	@OneToMany(cascade = CascadeType.ALL)
 	public Set<Adres> getAdressen() {
 		return adressen;
 	}
-	/**
-	 * @param adressen the adressen to set
-	 */
-	@SuppressWarnings("unused")
-	public void setAdressen(Set<Adres> adressen) {
+
+	private void setAdressen(Set<Adres> adressen) {
 		this.adressen = adressen;
 	}
-	
+
+	public boolean addNfa(Nfa nfa) {
+		return getNfaLijst().add(nfa);
+	}
+
+	public boolean removeNfa(Nfa nfa) {
+		return getNfaLijst().remove(nfa);
+	}
+
+	/**
+	 * Voegt een adres toe aan de lijst. Garandeert dat er precies 1
+	 * correspondentieadres aanwijzig is, tenzij de lijst leeg is.
+	 * 
+	 * @param adres
+	 *            Het toe te voegen adres.
+	 * @return True als toevoegen gelukt is, anders false.
+	 */
 	public boolean addAdres(Adres adres) {
 		if (adres != null && !heeftAdres(adres)) {
-			return getAdressen().add(adres);
+			if (getCorrespondentieAdres() == null) {// lijst is dan dus leeg
+				getAdressen().add(adres);// moet in de lijst zitten voor:
+				adres.maakCorrespondentieAdres(this);// doet niets als dit al
+														// true is
+				return heeftAdres(adres);
+			}// als code hier komt: getCorrespondentieAdres != null
+			else if (adres.isCorrespondentieAdres()) {
+				Adres correspondentieAdresOud = getCorrespondentieAdres();
+				getAdressen().add(adres);// nu dus 2 correspondentieadressen
+				correspondentieAdresOud.geenCorrespondentieAdres(this);
+				return heeftAdres(adres) && getCorrespondentieAdres() == adres;
+			} else {
+				return getAdressen().add(adres);
+			}
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Verwijderd een adres uit de lijst. Verwijderd enkel een gegeven adres
+	 * indien dit niet het correspondentieadres is. Maak eerst van een ander
+	 * adres uit de lijst een correspondentieadres als het te verwijderen adres
+	 * nog het correspondentieadres is. Als het te verwijderen adres een
+	 * correspondentieadres is en het is het enige adres van deze relatie, dan
+	 * kan deze via de methode verwijderLaatsteAdres() worden verwijderd.
+	 * 
+	 * @param adres
+	 *            Het toe te voegen adres, mag geen correspondentieadres zijn.
+	 * @return True als toevoegen gelukt is, anders false.
+	 */
+	public boolean removeAdres(Adres adres) {
+		if (adres.isCorrespondentieAdres() || !heeftAdres(adres)) {
+			return false;
+		} else {
+			return getAdressen().remove(adres);
+		}
+	}
+
+	/**
+	 * Methode die enkel gebruikt kan worden voor het verwijderen van het
+	 * laatste adres van deze relatie.
+	 * 
+	 * @return True als er maar 1 adres in de lijst voorkomt, anders false.
+	 */
+	public boolean verwijderLaatsteAdres() {
+		if (getAdressen().size() == 1) {
+			setAdressen(new HashSet<Adres>());
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	public boolean heeftAdres(Adres adres) {
 		return getAdressen().contains(adres);
 	}
-//	TODO
-//	public Adres getCorrespondentieAdres() {
-//		
-//	}
-//	
+
+	/**
+	 * Methode die het enige adres uit de lijst dat correspondentieadres is
+	 * teruggeeft.
+	 * 
+	 * @return Het correspondentieadres
+	 */
+	@Transient
+	public Adres getCorrespondentieAdres() {
+		for (Adres a : getAdressen()) {
+			if (a.isCorrespondentieAdres()) {
+				return a;
+			}
+		}
+		return null;
+	}
 
 }
