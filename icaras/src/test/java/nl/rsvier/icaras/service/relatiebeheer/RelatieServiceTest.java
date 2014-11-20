bed297c8086c832888d5ed41e02aa620f9bab586
Mark van Meerten <markvanmeerten@gmail.com>
2014-11-20 17:47:25 +0100

    opgeschoont, extra asserts om te kijken wat de effecten van de testen zijn op de adressen

--- icaras/src/test/java/nl/rsvier/icaras/service/relatiebeheer/RelatieServiceTest.java
+++ icaras/src/test/java/nl/rsvier/icaras/service/relatiebeheer/RelatieServiceTest.java
@@ -3,12 +3,9 @@ package nl.rsvier.icaras.service.relatiebeheer;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Set;
@@ -20,59 +17,69 @@ import nl.rsvier.icaras.core.relatiebeheer.Organisatie;
 import nl.rsvier.icaras.core.relatiebeheer.Persoon;
 import nl.rsvier.icaras.core.relatiebeheer.Relatie;
 import nl.rsvier.icaras.core.relatiebeheer.Werknemer;
-import nl.rsvier.icaras.dao.relatiebeheer.RelatieDaoHibernate;
+import nl.rsvier.icaras.dao.relatiebeheer.IAdresDao;
+import nl.rsvier.icaras.dao.relatiebeheer.IRelatieDao;
 
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//TODO embedded database voor deze test
+
+//TODO Embedded database voor deze test
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:icarasdb-context.xml" })
-//@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 public class RelatieServiceTest {
 
 	@Autowired
 	public IRelatieService service;
 
-	private Persoon relatie1;
-	private Organisatie relatie2;
-	private Adres adresRelatie1 = new Adres();
-	private Adres adresRelatie2 = new Adres();
-//	static Runtime rt = Runtime.getRuntime();
-//	static String executeBackupCmd = "/C:\\MySQL\\MySQL Server 5.6\\bin\\mysqldump -u" + "root" + " -p" + "masterkey" + " icaras -r " +  "C:\\icaras\\icarasbackup.sql";
-//	String execRestoreCmd = "/C:\\MySQL\\MySQL Server 5.6\\bin\\mysql -u" + "root" + " -p" + "masterkey" + " icaras < " +  "C:\\icaras\\icarasbackup.sql";
-	private static ArrayList<String> klasseNamen = new ArrayList<String>();
+	@Autowired
+	private IAdresDao adresDao;
 
-	
-//	@BeforeClass
-//	public static void init(){
-//		try{
-//		 rt.exec(executeBackupCmd);
-//		}
-//		catch(IOException e){
-//			System.out.println(e.getMessage());
-//		}
-//	}
-	
+	@Autowired
+	private IRelatieDao relatieDao;
+
+	private Persoon relatie1;
+	private Persoon relatie1_clone;
+
+	private Organisatie relatie2;
+	private Organisatie relatie2_clone;
+
+	private Adres adresRelatie1;
+	private Adres adresRelatie1_clone;
+	private Adres adresRelatie2;
+	private Adres adresRelatie2_clone;
+	private Adres adresRelatie3;
+	private Adres adresRelatie3_clone;
+
 	@Before
 	public void setUp() throws InvalidBusinessKeyException {
-//		//roep mysqldump aan om gemaakte mysql dump backup te loaden
-//		String comando = "C:\\MySQL\\MySQL server 5.6\\bin\\mysql.exe  --host=localhost --port=3306 --user=root --password=masterkey < C:\\icaras\\icarasbackup.sql";
-//		File f = new File("restore.bat");
-//		try{
-//		FileOutputStream fos = new FileOutputStream(f);
-//		fos.write(comando.getBytes());
-//		fos.close();
-//		Process run = Runtime.getRuntime().exec("cmd /C start restore.bat ");  
-//		}
-//		catch(Exception e){
-//			System.out.println("errorerror");
-//		}
+
+		adresRelatie1 = new Adres();
+		adresRelatie1.maakStraat();
+		adresRelatie1.setHuisOfPostbusNummer("456");
+		adresRelatie1.setPlaats("Volendam");
+		adresRelatie1.setPostcode("4565AK");
+		adresRelatie1.setStraat("palingstraat");
+
+		adresRelatie2 = new Adres();
+		adresRelatie2.maakPostbus();
+		adresRelatie2.setHuisOfPostbusNummer("1000");
+		adresRelatie2.setPlaats("Wageningen");
+		adresRelatie2.setPostcode("6700AA");
+		adresRelatie2.setStraat("Bugs Bunnystraat");
+
+		adresRelatie3 = new Adres();
+		adresRelatie3.maakPostbus();
+		adresRelatie3.setHuisOfPostbusNummer("3423");
+		adresRelatie3.setPlaats("Hilversum");
+		adresRelatie3.setPostcode("1000AB");
+		adresRelatie3.setStraat("blablastraat");
+
 		relatie1 = new Persoon();
 		relatie1.setAchternaam("Smit");
 		relatie1.setVoornaam("Jantje");
@@ -80,145 +87,179 @@ public class RelatieServiceTest {
 		relatie1.setGeboortedatum(new GregorianCalendar(1989, 9, 23));
 		relatie1.setOpmerking("Beroerde zanger");
 		relatie1.addRol(new Werknemer());
-
-		adresRelatie1.maakStraat();
-		adresRelatie1.setHuisOfPostbusNummer("456");
-		adresRelatie1.setPlaats("Volendam");
-		adresRelatie1.setPostcode("4565AK");
-		adresRelatie1.setStraat("palingstraat");
 		relatie1.addAdres(adresRelatie1);
 
 		relatie2 = new Organisatie("ACME");
 		relatie2.addRol(new Leverancier());
 		relatie2.setOpmerking("A company that manufactures EVERYTHING!");
-
-		adresRelatie2.maakPostbus();
-		adresRelatie2.setHuisOfPostbusNummer("1000");
-		adresRelatie2.setPlaats("Wageningen");
-		adresRelatie2.setPostcode("6700AA");
-		adresRelatie2.setStraat("Bugs Bunnystraat");
-
 		relatie2.addAdres(adresRelatie2);
+		relatie2.addAdres(adresRelatie3);
 
+		/*
+		 * Maak een kopie van elke relatie zodat we deze kunnen re-inserten
+		 * (zonder id dus) na een delete
+		 */
+
+		adresRelatie1_clone = new Adres();
+		adresRelatie1_clone.maakStraat();
+		adresRelatie1_clone.setHuisOfPostbusNummer("456");
+		adresRelatie1_clone.setPlaats("Volendam");
+		adresRelatie1_clone.setPostcode("4565AK");
+		adresRelatie1_clone.setStraat("palingstraat");
+
+		adresRelatie2_clone = new Adres();
+		adresRelatie2_clone.maakPostbus();
+		adresRelatie2_clone.setHuisOfPostbusNummer("1000");
+		adresRelatie2_clone.setPlaats("Wageningen");
+		adresRelatie2_clone.setPostcode("6700AA");
+		adresRelatie2_clone.setStraat("Bugs Bunnystraat");
+
+		adresRelatie3_clone = new Adres();
+		adresRelatie3_clone.maakPostbus();
+		adresRelatie3_clone.setHuisOfPostbusNummer("3423");
+		adresRelatie3_clone.setPlaats("Hilversum");
+		adresRelatie3_clone.setPostcode("1000AB");
+		adresRelatie3_clone.setStraat("blablastraat");
+
+		relatie1_clone = new Persoon();
+		relatie1_clone.setAchternaam("Smit");
+		relatie1_clone.setVoornaam("Jantje");
+		relatie1_clone.setTussenvoegsels("de");
+		relatie1_clone.setGeboortedatum(new GregorianCalendar(1989, 9, 23));
+		relatie1_clone.setOpmerking("Beroerde zanger");
+		relatie1_clone.addRol(new Werknemer());
+		relatie1_clone.addAdres(adresRelatie1_clone);
+
+		relatie2_clone = new Organisatie("ACME");
+		relatie2_clone.addRol(new Leverancier());
+		relatie2_clone.setOpmerking("A company that manufactures EVERYTHING!");
+		relatie2_clone.addAdres(adresRelatie2_clone);
+		relatie2_clone.addAdres(adresRelatie3_clone);
+
+		/*
+		 * Save de relatie
+		 */
+
+		service.save(relatie1);
+		service.save(relatie2);
 	}
 
-
-	private void flushAndClear(){
-//		service.getDao().getHibernateTemplate().flush();
-//		service.getDao().getHibernateTemplate().clear();
+	@After
+	public void tearDown() {
+		/**
+		 * Schoon de tabel op na elke test zodat deze bruikbaar is in een
+		 * volgende test
+		 */
+		service.delete(relatie1);
+		service.delete(relatie2);
 	}
-
-//	@Test
-//	public void testInitialisatie() {
-//		assertNotNull("RelatieService is null", service);
-//
-//		RelatieDaoHibernate dao = service.getDao();
-//
-//		assertNotNull("service.dao is null", dao);
-//
-//	}
 
 	@Test
 	public void testSaveEnGet() {
-		service.save(relatie1);
-		service.save(relatie2);
-//		service.getDao().getHibernateTemplate().flush();
-//		service.getDao().getHibernateTemplate().clear();
-		
 
-		Persoon returnRelatie1 = (Persoon) service.getById(relatie1
-				.getId());
-		Organisatie returnRelatie2 = (Organisatie) service.getById(relatie2
-				.getId());
+		relatie1 = (Persoon) service.getById(relatie1.getId());
+		relatie2 = (Organisatie) service.getById(relatie2.getId());
+
 		assertTrue("gesavede relatie1 is gelijke aan opgehaalde relatie",
-				relatie1.equals(returnRelatie1));
+				relatie1.equals(relatie1));
 		assertTrue("gesavede relatie2 is gelijke aan opgehaalde relatie",
-				relatie2.equals(returnRelatie2));
-		service.delete(relatie1);
-		service.delete(relatie2);
+				relatie2.equals(relatie2));
 
 	}
 
 	@Test
 	public void testUpdateVelden() {
 
-		service.save(relatie1);
-		service.save(relatie2);
-//		service.getDao().getHibernateTemplate().flush();
-
 		relatie1.setVoornaam("Kees");
 		relatie2.setOpmerking("Bijna alles");
 
 		service.update(relatie1);
 		service.update(relatie2);
-//		service.getDao().getHibernateTemplate().flush();
-//		service.getDao().getHibernateTemplate().clear();
 
-		Persoon relatie1Updated = (Persoon) service.getById(relatie1
-				.getId());
-		Organisatie relatie2Updated = (Organisatie) service.getById(relatie2
-				.getId());
+		relatie1 = (Persoon) service.getById(relatie1.getId());
+		relatie2 = (Organisatie) service.getById(relatie2.getId());
 
 		assertTrue("opgehaalde relatie1 gelijk aan relatie1Updated",
-				relatie1.equals(relatie1Updated));
+				relatie1.equals(relatie1));
 		assertTrue("opgehaalde relatie2 gelijk aan relatie2Updated Object",
-				relatie2.equals(relatie2Updated));
-		service.delete(relatie1);
-		service.delete(relatie2);
-	}
-
-	@Test
-	public void testUpdateList() {
-		// TODO testUpdateList, in afwachting van equals functies van persoon
-		// die ook lijsten meenemen
+				relatie2.equals(relatie2));
 
 	}
-	
+
 	@Test
 	public void testDeleteAdresVanRelatie() {
-		service.save(relatie1);
-		Persoon dbRelatie = (Persoon) service.getByIdMetAdres(relatie1.getId());
-		dbRelatie.verwijderLaatsteAdres();
-		service.update(dbRelatie);
-		Persoon dbPersNaUpdate = (Persoon) service.getByIdMetAdres(relatie1.getId());
-		assertTrue(dbPersNaUpdate.getAdressen().isEmpty());
-		service.delete(relatie1);		
+
+		relatie1 = (Persoon) service.getByIdMetAdres(relatie1.getId());
+		relatie2 = (Organisatie) service.getByIdMetAdres(relatie2.getId());
+
+		relatie1.verwijderLaatsteAdres();
+
+		Adres tmp = null;
+		for (Adres adres : relatie2.getAdressen()) {
+			if (!adres.isCorrespondentieAdres()) {
+				tmp = adres;
+			}
+		}
+		assertTrue("Adres kan niet worden verwijderd",
+				relatie2.removeAdres(tmp));
+
+		service.update(relatie1);
+		service.update(relatie2);
+
+		assertNotNull("Relatie kan niet worden gevonden",
+				relatie1 = (Persoon) service.getByIdMetAdres(relatie1.getId()));
+		assertNotNull(
+				"Relatie kan niet worden gevonden",
+				relatie2 = (Organisatie) service.getByIdMetAdres(relatie2
+						.getId()));
+
+		assertTrue("Relatie bevat nog een adres", relatie1.getAdressen().isEmpty());
+		assertSame("Relatie ", 1, relatie2.getAdressen().size());
+
 	}
 
 	@Test
 	public void testDelete() {
-		service.save(relatie1);
-		service.save(relatie2);
-//		flushAndClear();
+
+		relatie1 = (Persoon) service.getByIdMetAdres(relatie1.getId());
+		relatie2 = (Organisatie) service.getByIdMetAdres(relatie2.getId());
 
 		service.delete(relatie1);
 		service.delete(relatie2);
-//		flushAndClear();
 
 		assertNull("Gedelete relatie1 is null",
 				service.getById(relatie1.getId()));
 		assertNull("Gedelete relatie2 is null",
 				service.getById(relatie2.getId()));
 
+		assertNull(
+				"Controleer of de adressen die bij de relatie horen ook zijn verwijderd",
+				adresDao.getById(relatie1.getCorrespondentieAdres().getId()));
+		assertSame(
+				"Controleer of de adressen die bij de relatie horen ook zijn verwijderd",
+				0, adresDao.getAll().size());
+
+		service.save(relatie1_clone);
+		service.save(relatie2_clone);
+
+		relatie1 = relatie1_clone;
+		relatie2 = relatie2_clone;
 	}
 
 	@Test
 	public void testGetAll() {
-		service.save(relatie1);
-		service.save(relatie2);
-//		flushAndClear();
 
 		List<Relatie> relatieLijst = service.getAll();
 
 		assertTrue("relatieLijst bevat " + relatieLijst.size()
 				+ " relaties, dit zou 2 moeten zijn", relatieLijst.size() == 2);
+
 		Relatie[] relatieArray = new Relatie[relatieLijst.size()];
 		Persoon returnRelatie1 = null;
 		Organisatie returnRelatie2 = null;
 
 		for (Relatie r : relatieLijst.toArray(relatieArray)) {
 			if (r instanceof Persoon) {
-
 				returnRelatie1 = (Persoon) r;
 			} else if (r instanceof Organisatie) {
 				returnRelatie2 = (Organisatie) r;
@@ -229,15 +270,11 @@ public class RelatieServiceTest {
 				returnRelatie1.equals(relatie1));
 		assertTrue("Relatie2 uit lijst is niet gelijk aan relatie2",
 				returnRelatie2.equals(relatie2));
-		service.delete(relatie1);
-		service.delete(relatie2);
+
 	}
 
 	@Test
 	public void testGetAllMetAdres() {
-		service.save(relatie1);
-		service.save(relatie2);
-//		flushAndClear();
 
 		List<Relatie> relatieLijst = service.getAllMetAdres();
 
@@ -270,15 +307,11 @@ public class RelatieServiceTest {
 				(relatie1.getAdressen()).equals(relatie1Adressen));
 		assertTrue("relatie2 adressen niet gelijk aan returnRelatie2 Adressen",
 				(relatie2.getAdressen()).equals(relatie2Adressen));
-		service.delete(relatie1);
-		service.delete(relatie2);
+
 	}
 
 	@Test
 	public void testGetRelatieAdressen() {
-		service.save(relatie1);
-		service.save(relatie2);
-//		flushAndClear();
 
 		Set<Adres> relatie1Adressen = service.getRelatieAdressen(relatie1);
 		Set<Adres> relatie2Adressen = service.getRelatieAdressen(relatie2);
@@ -291,15 +324,13 @@ public class RelatieServiceTest {
 		assertTrue(
 				"relatie2 adressen set is niet gelijk aan relatie1.getadressen",
 				relatie2Adressen.equals(relatie2.getAdressen()));
-		service.delete(relatie1);
-		service.delete(relatie2);
+
 	}
 
 	@Test
 	public void testGetByIdMetAdressen() {
-		service.save(relatie1);
-		service.save(relatie2);
-//		flushAndClear();
+
+		int verwachtAantal = -1;
 
 		Persoon returnRelatie1MetAdres = (Persoon) service
 				.getByIdMetAdres(relatie1.getId());
@@ -309,21 +340,34 @@ public class RelatieServiceTest {
 		assertFalse(
 				"ReturnRelatie1MetAdressen heeft geen adressen, moet wel zo zijn",
 				returnRelatie1MetAdres.getAdressen().isEmpty());
+		verwachtAantal = relatie1.getAdressen().size();
+		assertSame(
+				String.format(
+						"We verwachten %s element, we hebben er meer of minder gevonden",
+						verwachtAantal), verwachtAantal, returnRelatie1MetAdres
+						.getAdressen().size());
+
 		assertFalse("returnRelatie2MetAdressen heeft geen adressen",
 				returnRelatie2MetAdres.getAdressen().isEmpty());
+		verwachtAantal = relatie2.getAdressen().size();
+		assertSame(
+				String.format(
+						"We verwachten %s element, we hebben er meer of minder gevonden",
+						verwachtAantal), verwachtAantal, returnRelatie2MetAdres
+						.getAdressen().size());
 
 		Set<Adres> returnAdressen1 = returnRelatie1MetAdres.getAdressen();
 		Set<Adres> returnAdressen2 = returnRelatie2MetAdres.getAdressen();
 		Set<Adres> relatie1Adressen = relatie1.getAdressen();
 		Set<Adres> relatie2Adressen = relatie2.getAdressen();
+
 		assertTrue(
 				"adressen returnRelatie1 zijn niet gelijk aan die van relatie1",
 				returnAdressen1.equals(relatie1Adressen));
 		assertTrue(
 				"adressen returnRelatie2 zijn niet gelijk aan die van relatie2",
 				returnAdressen2.equals(relatie2Adressen));
-		service.delete(relatie1);
-		service.delete(relatie2);
+
 	}
 
 }
