be85220a426b8fd59f8c85b230fff6a2d105b535
Gordon Brouwer <realgordon@gmail.com>
2014-11-24 23:42:16 +0100

    OrganisatieService en PersoonService toegevoegd.

---
+++ icaras/src/main/java/nl/rsvier/icaras/service/relatiebeheer/IOrganisatieService.java
@@ -0,0 +1,30 @@
+package nl.rsvier.icaras.service.relatiebeheer;
+
+import java.util.List;
+
+import nl.rsvier.icaras.core.relatiebeheer.Organisatie;
+
+public interface IOrganisatieService {
+
+	public void save(Organisatie organisatie);
+	
+	public void delete(Organisatie organisatie);
+	
+	public void update(Organisatie organisatie);
+	
+	public Organisatie getById(int id);
+	
+	public Organisatie getByIdMetAdressenEnNfaLijst(int id);
+	
+	public Organisatie getByIdMetRollen(int id);
+	
+	public Organisatie getByIdCompleet(int id);
+
+	public List<Organisatie> getAll();
+	
+	public List<Organisatie> getAllMetAdressenEnNfaLijst();	
+	
+	public List<Organisatie> getAllMetRollen();
+	
+	public List<Organisatie> getAllCompleet();
+}
---
+++ icaras/src/main/java/nl/rsvier/icaras/service/relatiebeheer/OrganisatieService.java
@@ -0,0 +1,74 @@
+package nl.rsvier.icaras.service.relatiebeheer;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import nl.rsvier.icaras.core.relatiebeheer.Organisatie;
+import nl.rsvier.icaras.dao.relatiebeheer.IOrganisatieDao;
+@Service("IOrganisatieService")
+@Transactional
+public class OrganisatieService implements IOrganisatieService {
+
+	@Autowired
+	private IOrganisatieDao organisatieDao;
+	
+	@Override
+	public void save(Organisatie organisatie) {
+		organisatieDao.save(organisatie);
+	}
+
+	@Override
+	public void delete(Organisatie organisatie) {
+		organisatieDao.delete(organisatie);
+	}
+
+	@Override
+	public void update(Organisatie organisatie) {
+		organisatieDao.update(organisatie);
+	}
+
+	@Override
+	public Organisatie getById(int id) {
+		return organisatieDao.getById(id);
+	}
+
+	@Override
+	public Organisatie getByIdMetAdressenEnNfaLijst(int id) {
+		return organisatieDao.getByIdMetAdressenEnNfaLijst(id);
+	}
+
+	@Override
+	public Organisatie getByIdMetRollen(int id) {
+		return organisatieDao.getByIdMetRollen(id);
+	}
+
+	@Override
+	public Organisatie getByIdCompleet(int id) {
+		return organisatieDao.getByIdCompleet(id);
+	}
+
+	@Override
+	public List<Organisatie> getAll() {
+		return organisatieDao.getAll();
+	}
+
+	@Override
+	public List<Organisatie> getAllMetAdressenEnNfaLijst() {
+		return organisatieDao.getAllMetAdressenenNfaLijst();
+	}
+
+	@Override
+	public List<Organisatie> getAllMetRollen() {
+		return organisatieDao.getAllMetRollen();
+	}
+
+	@Override
+	public List<Organisatie> getAllCompleet() {
+		return organisatieDao.getAllCompleet();
+	}
+
+}
